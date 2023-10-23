1d8574683185cecf231ff3b661d02c68d6109f15
sbazhko <andrew.glbv1997@gmail.com>
2023-10-23 21:51:21 +0200

    feat: add cv layout

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,11 +5,8 @@ const config: GatsbyConfig = {
     title: `siarhei-bazhko`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", {
+  plugins: ["gatsby-plugin-postcss", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -25,22 +25,7 @@ const codeStyles = {
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <h1 style={headingStyles}>Sorry 😔, we couldn't find what you were looking for.</h1>
   )
 }
 
